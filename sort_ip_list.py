e4c2b0c5cb78f9b5203e9c7f9ce6e205b9e0e25a
ubuntu <ubuntu@localhost.localdomain>
2018-10-25 13:40:11 +0800

    add sort ip list case, which can use ip list generated from last one

---
+++ sort_ip_list.py
@@ -0,0 +1,95 @@
+#This sorts the IP list by times 
+#alse sort by ip location
+# Version1
+# need to be optimized
+#from __future__ import print_function
+import urllib2
+import time 
+import sys
+import json
+def ip_to_country(ip):
+    y = {}
+    x = {}
+    for y in urllib2.urlopen(ip):
+        pass
+    x = json.loads(y)
+#    print x
+    time.sleep(0.5)
+    if "fail" in y:
+	return x["status"]
+    else:
+        return x["country"]
+
+
+
+f_ip = open("sort_by_ip.txt", "r+")
+f_cn = open("sort_by_cn.txt", "r+")
+f = open("ip.txt", 'r')
+words = []
+for line in f:
+    words.append(line)
+words.sort()
+for word in words:
+    pass
+
+print "=============================================="
+cnt = 0;
+cn_total = []
+i = 0
+flag = 0
+a=dict()
+b=dict()
+l=list()
+m=list()
+for c in words:
+    a[c]=a.get(c,0) + 1
+
+
+for ip, num in list(a.items()):#this is key 
+    l.append((num, ip))
+
+l.sort(reverse=True)
+for num, ip in l:
+    ip=ip.strip()
+    print(ip)
+    print(num)
+    f_ip.write(str(ip))
+    f_ip.write(" ")
+    f_ip.write(str(num))
+    f_ip.write("\n")
+    
+addr_pre = 'http://ip-api.com/json/'
+#print(a.items())
+for num, ip in l:
+    country = ip_to_country(addr_pre+ip)
+    if country not in b:
+        b[country]=num
+    else:
+        b[country]+=num
+
+for cn,num in list(b.items()):
+    m.append((num, cn))
+
+m.sort(reverse=True)
+for num, cn in m:
+    f_cn.write(str(cn))
+    f_cn.write(" ")
+    f_cn.write(str(num))
+    f_cn.write("\n")
+    #print(cn),
+    #print(num)
+
+exit()
+print(b)
+for cn, num in l:
+    print(cn),
+    print(num)
+    try:
+    	f_ip.write(str(country))
+    except:
+    	f_ip.write("Unknown")
+
+print "=============================================="
+f_ip.close()
+f_cn.close()
+f.close()
