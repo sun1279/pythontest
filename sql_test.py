867ce2724fffe138ad5fb1a42042181cce0df55e
sun1279 <266211@gmail.com>
2019-05-13 09:59:36 +0800

    sql test

--- sql_test.py
+++ sql_test.py
@@ -3,18 +3,39 @@ import sqlite3
 conn = sqlite3.connect('test.sqlite')
 cur = conn.cursor()
 
+try:
+    cur.execute('SELECT * FROM mytest')
+except:
+    cur.execute('CREATE TABLE mytest (title,num)')
 
 cur.execute('INSERT INTO mytest (title, num) VALUES(?,?)',('Sunny',11))
 cur.execute('INSERT INTO mytest (title, num) VALUES(?,?)',('Su',13))
 cur.execute('INSERT INTO mytest (title, num) VALUES(?,?)',('Sunn',26))
 cur.execute('INSERT INTO mytest (title, num) VALUES(?,?)',('Sunny',22))
+cur.execute('INSERT INTO mytest (title, num) VALUES(?,?)',('hise',52))
+cur.execute('INSERT INTO mytest (title, num) VALUES(?,?)',('hisense',222))
+cur.execute('INSERT INTO mytest (title, num) VALUES(?,?)',('Hisen',112))
 conn.commit()
 print("Write finished\n")
 #cur.execute('SELECT title, num FROM mytest')
-cur.execute('SELECT * FROM mytest WHERE title = "Sunny"')
-for row in cur:
-    print(row)
+title=('Sunny',)
+cur.execute('SELECT * FROM mytest WHERE title =?', title)
+#print(cur.fetchone())
+print(cur.fetchall())
+#for row in cur:
+#    print(row)
 
-cur.execute('DELETE FROM mytest WHERE num < 100')
+for r in cur.execute('SELECT * FROM mytest ORDER BY num'):
+    print(r)
+
+num=222
+cur.execute('DELETE FROM mytest WHERE num =?',(num,))
+num=11
+cur.execute('DELETE FROM mytest WHERE num =?',(num,))
+cur = conn.cursor()
+print("================")   
+cur.execute('SELECT * FROM mytest')
+for i in cur:
+    print(i)
 conn.commit()
 conn.close()
