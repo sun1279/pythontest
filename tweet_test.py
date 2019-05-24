143f7b3214e806abe6c5495ba46a9a53bf7606e5
sun1279 <266211@gmail.com>
2019-05-24 16:49:46 +0800

    modify tweet_test

--- tweet_test.py
+++ tweet_test.py
@@ -3,6 +3,7 @@ import re
 import shutil
 import requests
 from requests_html import HTMLSession
+import time
 
 headers = {
     'Referer': 'https://twitter.com',
@@ -12,95 +13,154 @@ headers = {
 
 session = HTMLSession()
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_KEY = '-zxsc89YPrumdIKvdB9Gyw6TvrNnN4E08zhQ1lMeT'
-ACCESS_SECRET = ''
+CONSUMER_KEY = 'z5mK2uctiBdV4BHEWrpenqd77'
+CONSUMER_SECRET = '9OcFgySBeWzApYYLmkUqg25vYFwj2pXedSnR1Sr2c7dF4Akt7d'
+ACCESS_KEY = '161683541-zxsc89YPrumdIKvdB9Gyw6TvrNnN4E08zhQ1lMeT'
+ACCESS_SECRET = 'curi1UsKb3KUzql6eSmiAKDyeChf3BCb4BykcIN5e967L'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-#api.update_status('HELLO World')
-#api.update_with_media('rtos.png', "helloworld")
-
-ls = list(api.home_timeline(count=100))
-#ls = list(api.user_timeline("nytchinese"))
-
-#get full tweet text
-def get_full_text(url):
-    try:
-        r = session.get(url, headers=headers)
-        newurl = r.html.text
-        newurl = newurl.split('\n')[0]
-        r = session.get(newurl, headers=headers)
-        text = r.html.find('title')[0].text.split("\"")[1]
-    except:
-        print("Get Full content error")
-        text=''
-    return text
 
 
-#download media file
-def download_media(url, filename):
-    print("DDDDDDDDDDDDDDDDDDOWNLOAD")
-    exit()
-    try:
-        r = requests.get(url, stream=True, headers=headers)
-        with open(filename, 'wb') as fd:
-            shutil.copyfileobj(r.raw, fd)
-    except:
-        print("Download error")
+class MyTweet(object):
 
-#with open('tt.pickle', 'rb') as fd:
-#    ls=pickle.load(fd)
+    def __init__(self, api):
+        self.__api__ = api
+
+        #download media file
+    def download_media(self, url, filename):
+        print("DDDDDDDDDDDDDDDDDDOWNLOAD")
+        exit()
+        try:
+            r = requests.get(url, stream=True, headers=headers)
+            with open(filename, 'wb') as fd:
+                shutil.copyfileobj(r.raw, fd)
+        except:
+            print("Download error")
 
 
-for l in ls:
-    print("========================================================")
-    print("name: {}".format(l.author.name))
-    print("Screen_name: {}".format(l.author.screen_name))
-    print("--------------------------------------------------------")
-    print("Description: {}".format(l.author.description))
-    print("--------------------------------------------------------")
-    print("Created at: {}".format(l.created_at))
-    if l.truncated:  #if l.text is full content or not
-        full_content_url = re.findall('https\S+', l.text)[0]
-        print("NEED to preocess text")
-        full_content = get_full_text(full_content_url)
-        if full_content is '': #do not get full content
-            full_content=l.text
-        print(full_content)
 
-    else:
-        print("Noooooooooooooooo NEED to preocess text")
-        print(l.text)
+            
+    def home(self, cnt):
+        ls = list()
+        try:
+            ls = list(self.__api__.home_timeline(count=cnt))
+        except:
+            print("ERROR")
+        return ls
 
-    tweet_id = l.id_str
-    #get image/video
-    try:
-        if l.extended_entities:
-            print('Media')
-            media_lists = l.extended_entities['media']
+
+    def user(self, user, cnt):
+        ls = list()
+        try:
+            ls = list(self.__api__.user_timeline(user,count=cnt))
+        except:
+            print("ERROR")
+        return ls
+
+    def send_tweet(self, text, media=''):
+        if media != '':
+            self.__api__.update_with_media(media, "helloworld Nice to meet ffff")
+        else:
+            self.__api__.update_status(text)
+
+    class Process(object):
+
+        def __init__(self, l):
+            self.__l__ = l
+        #get full tweet text
+        def get_full_text(self, url):
+            try:
+                r = session.get(url, headers=headers)
+                newurl = r.html.text
+                newurl = newurl.split('\n')[0]
+                r = session.get(newurl, headers=headers)
+                text = r.html.find('title')[0].text.split("\"")[1]
+            except:
+                print("Get Full content error")
+                text=''
+            return text
+    
+
+        def name(self):
+            return self.__l__.author.name
+
+        def screen_name(self):
+            return self.__l__.author.screen_name
+
+        def description(self):
+            return self.__l__.author.description
+
+        def create_at(self):
+            return self.__l__.created_at
+
+        def content(self):
+            if self.__l__.truncated:  #if l.text is full content or not
+                full_content_url = re.findall('https\S+', self.__l__.text)[0]
+                full_content = self.get_full_text(full_content_url)
+                if full_content is '': #do not get full content
+                    full_content=self.__l__.text
+            else:
+                full_content=self.__l__.text
+
+            return full_content
+        
+        def get_id(self):
+            return self.__l__.id_str
+
+        def get_media(self):
+            all_files=list()
+            try:
+                if self.__l__.extended_entities:
+                    media_lists = self.__l__.extended_entities['media']
+                    for media in media_lists:
+                        mediaurl = media['media_url_https']
+                        all_files.append(mediaurl)
+            except:
+                pass
+            return all_files
+
+        def download_media(self, url_list, id_str):
             media_cnt = 0
-            for media in media_lists:
-                mediatype = media['type']
-                mediaurl = media['media_url_https']
-                print(mediatype)
-                print(mediaurl)
-                postfix = mediaurl.split(".")[-1]
-                download_media(mediaurl, tweet_id+str(media_cnt)+'.'+postfix)
-                media_cnt += 1
-    except:
-        pass
+            for url in url_list:
+                postfix = url.split(".")[-1]
+                download_media(url, id_str+'_'+str(media_cnt)+'.'+postfix)
+            print("Download finished")
 
-    #for retweets, get original tweet info
-    if l.is_quote_status:
-        ids = list()
-        ids.append(l.quoted_status_id_str)
-        print("------------------retweet---------------------------------")
-        newuser = api.statuses_lookup(ids)
-        print("name: {}".format(newuser[0].author.name))
-        print("Screenname: {}".format(newuser[0].author.screen_name))
-        print("Desc: {}".format(newuser[0].author.description))
-        print("Created: {}".format(newuser[0].created_at))
-        print(newuser[0].text)
+        def is_retweet(self):
+            if self.__l__.is_quote_status:
+                return True
+            else:
+                return False
+
+        def orig_tweet(self):
+            d=dict()
+            if self.__l__.is_quote_status:
+                try :
+                    newuser=self.__l__.quoted_status
+                    d['Name'] = newuser.author.name
+                    d['ScreenName'] = newuser.author.screen_name
+                    d['Description'] = newuser.author.description
+                    d['Create'] = newuser.created_at
+                    d['Content'] = newuser.text
+                except:
+                    print("GET ORIG ERROR")
+                    pass
+            return d;
+
+
+
+t=MyTweet(api)
+#t.send_tweet("Current time:2018-9-10 1234")
+msg=t.home()
+for m in msg:
+    time.sleep(5)
+    print(t.Process(m).name())
+    print(t.Process(m).screen_name())
+    print(t.Process(m).description())
+    print(t.Process(m).create_at())
+    print(t.Process(m).content())
+    if t.Process(m).is_retweet():
+        print(t.Process(m).orig_tweet())
+
