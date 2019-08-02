ee268ec069340c2233e51765cc8fe26427412270
sun1279 <266211@gmail.com>
2019-08-02 02:14:11 -0400

    add text2hex.py

---
+++ text2hex.py
@@ -0,0 +1,22 @@
+import sys
+l=len(sys.argv)
+if l is not 3:
+    print("Usage: python3 text2hex.py input.h output.bin")
+    exit()
+
+try:
+    with open(sys.argv[1], 'r') as fd:
+        l=fd.read()
+except:
+    print("Open file ({}) failed".format(sys.argv[1]))
+    exit()
+all=list()
+for i in l.split(','):
+    try:
+        all.append(int(i,16))
+    except:
+        pass
+
+with open(sys.argv[2], 'wb') as fd:
+    fd.write(bytearray(all))
+
