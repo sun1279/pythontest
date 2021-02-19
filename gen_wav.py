23e09e49349cef389ada6d6553f70b5f8f4b8a1a
sun1279 <266211@gmail.com>
2021-02-19 16:20:50 +0800

    update gen_wav

--- gen_wav.py
+++ gen_wav.py
@@ -1,13 +1,13 @@
 import math
+import wave
 
-
-
+alt=32700
 def create_wav(name, sr, freq, time):
     length = (sr*time)
     fd=open(name, "wb+")
-     all_list=list()
+    all_list=list()
     for i in range(length):
-        d=math.sin((math.pi/180)*i*360/(sr/freq))
+        d=math.sin(2*(math.pi)*i/(sr/freq))
         data=round(d*alt)
         if(data < 0):
             data = ((1<<16)+data)
@@ -16,7 +16,11 @@ def create_wav(name, sr, freq, time):
         all_list.append(d1)
         all_list.append(d2)
 
+    pcmdata = bytearray(all_list)
     fd.write(bytearray(all_list))
     fd.close()
+    with wave.open(name+".wav", 'wb') as wavfile:
+        wavfile.setparams((1,2,16000,0, 'NONE', 'NONE'))
+        wavfile.writeframes(pcmdata)
 
-create_wav("1000hz.pcm", 16000, 1000, 10)
+create_wav("1000hz", 16000, 1000, 10)
